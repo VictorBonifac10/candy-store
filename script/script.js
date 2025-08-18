47094a0ec913399dd92da0baee38056829f644c9
victorbonifac10 <victor.alvesbonifacio@gmail.com>
2025-08-18 11:15:21 -0300

    Revisao de elementos e finalizacao da responsividade

--- script/script.js
+++ script/script.js
@@ -1,20 +1,25 @@
- $(document).ready(function() {
-            // Seleciona todos os contadores
-            const counters = $(".counter-section .count");
+// --> AOS Animate
 
-            // Cria observer
-            const observer = new IntersectionObserver((entries, obs) => {
-                entries.forEach(entry => {
-                    if(entry.isIntersecting) {
-                        const $this = $(entry.target);
-                        const target = parseInt($this.data("statistics_percent"));
-                        $this.animateNumber({ number: target }, 4000);
-                        obs.unobserve(entry.target); // garante que só anima uma vez
-                    }
-                });
-            }, { threshold: 0.5 }); // anima quando 50% visível
+AOS.init();
 
-            counters.each(function() {
-                observer.observe(this);
-            });
+// --> Numbers Animate
+
+$(document).ready(function() {
+    // Seleciona todos os contadores
+    const counters = $(".counter-section .count");
+    // Cria observer
+    const observer = new IntersectionObserver((entries, obs) => {
+        entries.forEach(entry => {
+            if(entry.isIntersecting) {
+            const $this = $(entry.target);
+            const target = parseInt($this.data("statistics_percent"));
+            $this.animateNumber({ number: target }, 4000);
+            obs.unobserve(entry.target); // garante que só anima uma vez
+            }
         });
+    }, { threshold: 0.5 }); // anima quando 50% visível
+
+    counters.each(function() {
+        observer.observe(this);
+    });
+});
