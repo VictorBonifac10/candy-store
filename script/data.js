6e1ede07f12dc1adfb63dc026a88f628d637ca4e
victorbonifac10 <victor.alvesbonifacio@gmail.com>
2025-09-12 15:42:12 -0300

    Criacao da base de dados e botao carrinho de compras

---
+++ script/data.js
@@ -0,0 +1,38 @@
+export const products = [
+    {
+        src: './assets/img/doce (1).jpg',
+        name: 'Bolos',
+        flavors: ['Floresta Negra', 'Morango Trufado', 'Laranja e Coco'],
+        price: 85
+    },
+    {
+        src: './assets/img/doce (2).jpg',
+        name: 'Tortas',
+        flavors: ['Limão', 'Morango', 'Chocolate '],
+        price: 55
+    },
+    {
+        src: './assets/img/doce (3).jpg',
+        name: 'Croissants',
+        flavors: ['Nutella', 'Doce de Leite'],
+        price: 15
+    },
+    {
+        src: './assets/img/doce (4).jpg',
+        name: 'Bolo de Pote',
+        flavors: ['Nutella', 'Lacta', 'Whey Protein', 'Prestigio'],
+        price: 12
+    },
+    {
+        src: './assets/img/doce (5).jpg',
+        name: 'Cone Trufado',
+        flavors: ['Nutella', 'Ferrero Rocher', 'M&Ms'],
+        price: 12
+    },
+    {
+        src: './assets/img/doce (6).jpg',
+        name: 'Morango do Amor',
+        flavors: ['c/ Brigadeiro Branco', 'c/ Brigadeiro de Maracujá'],
+        price: 15
+    }
+]
--- script/script.js
+++ script/script.js
@@ -1,25 +1,70 @@
+// --> Imports
+
+import { products } from './data.js'; // precisa do .js no final
+
 // --> AOS Animate
 
 AOS.init();
 
 // --> Numbers Animate
 
-$(document).ready(function() {
+$(document).ready(function () {
     // Seleciona todos os contadores
     const counters = $(".counter-section .count");
     // Cria observer
     const observer = new IntersectionObserver((entries, obs) => {
         entries.forEach(entry => {
-            if(entry.isIntersecting) {
-            const $this = $(entry.target);
-            const target = parseInt($this.data("statistics_percent"));
-            $this.animateNumber({ number: target }, 4000);
-            obs.unobserve(entry.target); // garante que só anima uma vez
+            if (entry.isIntersecting) {
+                const $this = $(entry.target);
+                const target = parseInt($this.data("statistics_percent"));
+                $this.animateNumber({ number: target }, 4000);
+                obs.unobserve(entry.target); // garante que só anima uma vez
             }
         });
     }, { threshold: 0.5 }); // anima quando 50% visível
 
-    counters.each(function() {
+    counters.each(function () {
         observer.observe(this);
     });
 });
+
+
+const catalog = document.querySelector('.catalog')
+
+products.forEach(element => {
+
+    const flavorOptions = element.flavors.map(flavor => `
+
+        <label class="flex items-center gap-2">
+            <input type="radio" name="sabor-${element.name}" value="${flavor}" class="accent-rose-400">
+            <span>${flavor}</span>
+        </label>
+    `).join('');
+
+    catalog.innerHTML += `
+                <div
+                    class="bg-white rounded-2xl shadow-lg overflow-hidden flex flex-col transition-all hover:scale-105">
+                    <img src="${element.src}" alt="Doce 1" class="w-full h-40 object-cover">
+                    <div class="p-4 flex flex-col justify-between flex-grow">
+                        <h3 class="font-lobster not-first:text-xl font-bold text-rose-400 mb-2">${element.name}</h3>
+                        <form class="flex flex-col space-y-2">
+                        ${flavorOptions}
+                        </form>
+                        <div class="flex justify-between items-center mt-auto gap-4">
+                            <button
+                                class="flex items-center justify-center w-12 h-12 bg-rose-400 text-white hover:bg-rose-500 rounded-full">
+                                <i class="ri-whatsapp-fill text-2xl"></i>
+                            </button>
+                            <button
+                                class="flex items-center justify-center w-12 h-12 bg-gray-200 text-rose-400 hover:bg-gray-300 rounded-full">
+                                <i class="ri-shopping-cart-fill text-2xl"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+`
+});
+
+
+
